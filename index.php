ce7fc1861a5368d44f5599d73268fdfb57017261
ArthurBC07 <arthurcardososjs@gmail.com>
2024-06-13 09:11:14 -0300

    Telas: Login e Home

--- index.php
+++ index.php
@@ -10,17 +10,39 @@ if (@$_SESSION['nome']){
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>FlipUFSC</title>
-</head>
-<body>
-    <h1>LOGIN</h1>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap">
+    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 
-    <form action="login.php" method="POST">
-        <input type="number" placeholder="Matrícula" name="matricula" required>
-        <input type="submit" name="btnLogin">
-    </form>
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <h1 class="title-flipufsc"><span class="flip">FLIP</span><span class="ufsc">UFSC</span></h1>
+        </div>
+        <div class="row">
+            <h1 class="title-login">LOGIN</h1>
+        </div>
+        <div class="row">
+            <form action="login.php" method="POST">
+                <div class="input-matricula">
+                    <i class="fa-solid fa-user"></i>
+                    <input type="number" class="login-matricula" placeholder="Matrícula" name="matricula" required>
+                </div>
+                <input class="login-submit" type="submit" name="btnLogin">
+            </form>
+        </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
+
 </html>
--- views/home.php
+++ views/home.php
@@ -6,19 +6,46 @@ include_once('../verifica.php');
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>HOME</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap">
+    <link rel="stylesheet" href="view.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
+
 <body>
-    <h1>Seja bem-vindo, <?php echo $_SESSION['nome'];?></h1>
-    <h3>PONTUAÇÃO: <?php echo $_SESSION['pontuacao']; ?></h3>
+    <div class="container">
+        <div class="row home-titulos">
+            <h1 class="title-home">Seja bem-vindo,<span class="home-nome"><?php echo $_SESSION['nome'];?></span></h1>
+        </div>
+        <div class="row home-pontos">
+            <h3 class="pontuacao-home">PONTOS:</h3>
+            <h2 class="home-pontuacao">
+                <?php echo isset($_SESSION['pontuacao']) && $_SESSION['pontuacao'] !== '' ? $_SESSION['pontuacao'] : 0; ?>
+            </h2>
+        </div>
 
-    <a href="questao.php">JOGAR</a>
-    <a href="ranking.php">RANKING</a>
+        <div class="row home-bottons">
+            <div class="home-jogar">
+                <a class="botton-home" href="questao.php">JOGAR</a>
+            </div>
+            <div class="home-ranking">
+                <a class="botton-home" href="ranking.php">RANKING</a>
+            </div>
+            <div class="home-sair">
+                <a class="botton-home" href="../logout.php">Sair do sistema</a>
+            </div>
+        </div>
+    </div>
 
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
-    <a href="../logout.php">Sair do sistema</a>
 </body>
+
 </html>

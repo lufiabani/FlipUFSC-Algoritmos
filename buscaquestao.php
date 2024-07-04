1fb98cf00b064eb379f8d4a2e737eb81ffc4f03c
Luís Antônio Fiabani <luisfiabani@gmail.com>
2024-07-04 20:18:51 -0300

    Alert Jogadas Esgotadas

--- buscaquestao.php
+++ buscaquestao.php
@@ -5,7 +5,6 @@ include_once('verifica.php');
 
 $usuario_id = $_SESSION['usuario_id'];
 
-// Consulta para contar as jogadas na data de hoje
 $sql_jogadas = "SELECT * FROM rodadas WHERE aluno_id = :usuario_id AND DATE(datahora) = CURDATE()";
 $stmt_jogadas = $pdo->prepare($sql_jogadas);
 $stmt_jogadas->bindParam(':usuario_id', $usuario_id, PDO::PARAM_INT);
@@ -13,16 +12,23 @@ $stmt_jogadas->execute();
 $resultado_jogadas = $stmt_jogadas->fetchAll(PDO::FETCH_ASSOC);
 
 if (count($resultado_jogadas) >= 2) {
-    header("Location: ../logout.php");
+    echo '<script>
+        alert("Você atingiu o limite de jogadas hoje, volte amanhã!");
+        window.location.href = "../logout.php";
+    </script>';
+    exit();
 } else {
+    
     $sql = "SELECT * FROM questoes";
     $stmt = $pdo->prepare($sql);
     $stmt->execute();
-    $questoes = $stmt->fetchAll();
+    $questoes = $stmt->fetchAll(PDO::FETCH_ASSOC);
     $questao = $questoes[array_rand($questoes)];
 
-    $sql = "SELECT * FROM alternativas WHERE (questao_id = $questao[id])";
+    $sql = "SELECT * FROM alternativas WHERE questao_id = :questao_id";
     $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(':questao_id', $questao['id'], PDO::PARAM_INT);
     $stmt->execute();
-    $alternativas = $stmt->fetchAll();
+    $alternativas = $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
+

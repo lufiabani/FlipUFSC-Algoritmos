4da1ce37fe70c9f952f0891365331ad228ce1307
Luís Antônio Fiabani <luisfiabani@gmail.com>
2024-07-04 19:51:33 -0300

    Limite Jogadas

--- buscaquestao.php
+++ buscaquestao.php
@@ -3,13 +3,26 @@
 include_once('bd.php');
 include_once('verifica.php');
 
-$sql = "SELECT * FROM questoes";
-$stmt = $pdo->prepare($sql);
-$stmt->execute();
-$questoes = $stmt->fetchAll();
-$questao = $questoes[array_rand($questoes)];
+$usuario_id = $_SESSION['usuario_id'];
 
-$sql = "SELECT * FROM alternativas WHERE (questao_id = $questao[id])";
-$stmt = $pdo->prepare($sql);
-$stmt->execute();
-$alternativas = $stmt->fetchAll();
+// Consulta para contar as jogadas na data de hoje
+$sql_jogadas = "SELECT * FROM rodadas WHERE aluno_id = :usuario_id AND DATE(datahora) = CURDATE()";
+$stmt_jogadas = $pdo->prepare($sql_jogadas);
+$stmt_jogadas->bindParam(':usuario_id', $usuario_id, PDO::PARAM_INT);
+$stmt_jogadas->execute();
+$resultado_jogadas = $stmt_jogadas->fetchAll(PDO::FETCH_ASSOC);
+
+if (count($resultado_jogadas) >= 2) {
+    header("Location: ../logout.php");
+} else {
+    $sql = "SELECT * FROM questoes";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute();
+    $questoes = $stmt->fetchAll();
+    $questao = $questoes[array_rand($questoes)];
+
+    $sql = "SELECT * FROM alternativas WHERE (questao_id = $questao[id])";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute();
+    $alternativas = $stmt->fetchAll();
+}
--- login.php
+++ login.php
@@ -2,22 +2,24 @@
 @session_start();
 include_once('bd.php');
 
-if(@$_POST['btnLogin']){
+if (@$_POST['btnLogin']) {
     $matricula = $_POST['matricula'];
-    $sql = "SELECT * FROM alunos WHERE (matricula = $matricula)";
+    $sql = "SELECT * FROM alunos WHERE matricula = :matricula";
     $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(':matricula', $matricula, PDO::PARAM_INT);
     $stmt->execute();
     $resultado = $stmt->fetchAll();
 
-    if(count($resultado) == 1){
+    if (count($resultado) == 1) {
         $_SESSION['nome'] = $resultado[0]['nome'];
         $_SESSION['usuario_id'] = $resultado[0]['id'];
         $_SESSION['pontuacao'] = $resultado[0]['pontuacao'];
+
         header("Location: views/home.php");
     } else {
         header("Location: index.php");
     }
-
 } else {
     header("Location: index.php");
 }
+?>
